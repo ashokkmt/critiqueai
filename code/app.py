00187492d26aebd8c1379017d51e6dcd0577879d
Ashok Kumawat <ashokkumawat4555@gmail.com>
2025-03-11 11:25:33 +0530

    app.py updated

--- code/app.py
+++ code/app.py
@@ -463,4 +463,4 @@ def evaluate():
 
 # Run the app in debug mode for development
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(debug=True)
