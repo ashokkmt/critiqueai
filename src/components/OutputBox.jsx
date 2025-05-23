db3e0d11d383c726814f15c85387bfbc3e850a7d
Ashok Kumawat <ashokkumawat4555gmail.com>
2025-05-23 13:05:22 +0530

    test 2

---
+++ src/components/OutputBox.jsx
@@ -0,0 +1,14 @@
+import '../styles/OutputBox.css'
+
+export default function OutputBox() {
+    return (
+        <div className='output-box'>
+            <div className='sub-out-box'>
+                <h3>Output</h3>
+                <div className='output-here'>
+                    Output Here....
+                </div>
+            </div>
+        </div>
+    )
+}
