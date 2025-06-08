38a53cf55c3f721b7e21f10670cd6416bc3ecda7
Ashok Kumawat <ashokkumawat4555@gmail.com>
2025-06-08 22:18:53 +0530

    remove non used libraries

--- code/app.py
+++ code/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, session, jsonify, redirect, url_for
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pymupdf
 import pdfplumber
 import os
-import json
 import docx
 import markdown2
 from werkzeug.utils import secure_filename
